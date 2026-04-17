0424d4f2ffa886bf4b99f949efaf838def7f334e
Batisli <liviardgbatista@gmail.com>
2026-04-17 20:30:54 -0300

    17/04/2026

--- DML_escola.sql
+++ DML_escola.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE EscolaDB;
 
-USE EscolaDB
+USE EscolaDB;
 
 --DROP TABLE Alunos;
 
@@ -10,5 +10,33 @@ USE EscolaDB
  Nome VARCHAR(200) NOT NULL,
  Email VARCHAR (200) UNIQUE
  );
-  INSERT INTO Alunos (,Nome,Idade ,Email)
-  VALUES (1, 'Lívia', 23, 'livia@gmail.com');
+  INSERT INTO Alunos (Nome,Idade ,Email)
+  VALUES ('Lívia', 23, 'livia@gmail.com');
+
+  INSERT INTO Alunos (Nome,Idade ,Email)
+  VALUES 
+  ('Joanna', 23, 'joanna@gmail.com'),
+  ('Emily', 23, 'emily@gamil.com');
+
+--Não rodar
+ -- update Alunos
+  --set idade = 19;
+
+
+    update Alunos
+  set idade = 23
+where Nome= 'Joanna';
+
+ --update Alunos
+  --set idade = 22
+--where Nome= 'Emily';
+
+INSERT INTO Alunos (Nome,Idade ,Email)
+  VALUES 
+  ('Joanna', 18, 'joanna1@gmail.com');
+
+  
+    update Alunos
+  set idade = 30
+where Nome= 'Joanna';
+
